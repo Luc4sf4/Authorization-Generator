9e1b6562a0cfdeadc0eec678c9c2cd16d4bdca9d
Luc4sf4 <superlucas2005@gmail.com>
2025-03-25 11:11:55 -0300

    Finish de login validation

--- src/main/java/br/com/AuthorizationGenereaor/Authorization/Generator/core/Controllers/TokenController.java
+++ src/main/java/br/com/AuthorizationGenereaor/Authorization/Generator/core/Controllers/TokenController.java
@@ -7,11 +7,15 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
 
 @RestController
 @AllArgsConstructor
@@ -26,9 +30,29 @@ public class TokenController {
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest){
         var user = userRepository.findByUsername(loginRequest.username());
 
+
+        //se o usuario for vazio e se o login nao bate, ele joga uma exceptions
         if(user.isEmpty()|| user.get().isLoginCorrect(loginRequest, passwordEncoder)){
             throw new BadCredentialsException("user or password is not valid");
         }
+
+        //configurando o tempo de expiracao
+        var now = Instant.now();
+        var expiresIn = 300L;
+
+
+        //montando o token do jwt
+        var claims = JwtClaimsSet.builder()
+                .issuer("mydbackend")
+                .subject(user.get().getUserId().toString())
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiresIn))
+                .build();
+
+        var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+
+        return ResponseEntity.ok(new LoginResponse(jwtValue, expiresIn));
+
 
     }
 
--- src/main/java/br/com/AuthorizationGenereaor/Authorization/Generator/core/DTOs/LoginResponse.java
+++ src/main/java/br/com/AuthorizationGenereaor/Authorization/Generator/core/DTOs/LoginResponse.java
@@ -1,4 +1,4 @@
 package br.com.AuthorizationGenereaor.Authorization.Generator.core.DTOs;
 
-public record LoginResponse(String accessToken, String expiresIn) {
+public record LoginResponse(String accessToken, Long expiresIn) {
 }
